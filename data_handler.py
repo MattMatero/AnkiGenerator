f447b27c7454980c037bdd06109bc15144bd007f
David Brosnan <dbrosn626@gmail.com>
2023-01-14 15:38:25 -0500

    pushing changes to merge matt

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,23 @@
 import pandas as pd
 from os import path
+import uuid
+
+
+def generate_uuid(df):
+
+    iters = 10
+
+    while iters > 0:
+
+        new_uuid = uuid.uuid4()
+
+        if len(df.index) == 0 or new_uuid not in df["uuid"]:
+            return new_uuid
+        
+        iters -= 1
+
+    return None
+
 
 def open_file(file_loc, columns=None):
     if path.exists(file_loc):
@@ -9,6 +27,40 @@ def open_file(file_loc, columns=None):
 
     return df
 
+def insert(df, list_data):
+    
+    pd.DataFrame.from_dict({'words'})
+
 def save_df(df, output_path):
     df.to_csv(output_path)
     return df
+
+
+def test():
+    words = open_file("./data/words.csv")
+
+    print(words)
+
+    print(words.columns)
+
+    name = "TEST_WORD"
+
+    df = pd.DataFrame.from_dict({'words': [name]})
+    #df = pd.DataFrame(d)
+
+    #print(df)
+
+    words = pd.concat([words, df], ignore_index=True)
+
+    print(words.head(10))
+    print(words.columns)
+
+def test1():
+
+    cards = open_file("./data/cards.csv")
+
+
+    print(generate_uuid(cards))
+
+if __name__ == "__main__":
+    test1()
