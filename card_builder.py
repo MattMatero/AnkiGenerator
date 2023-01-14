7d9b2fef59b989fa0bc7052302fc88e6648e47d5
Matt Matero <matthewmatero@mail.adelphi.edu>
2023-01-14 14:31:32 -0500

    multi sense v0.1

--- card_builder.py
+++ card_builder.py
@@ -1,29 +1,42 @@
 from jisho_api.word import Word
 from data_handler import open_file, save_df
+import pandas as pd
+import sys
 
 def query_word(word):
     r = Word.request(word)
     data = r.data[0]
+    res = []
 
     if len(data.japanese) == 1:
         reading = data.japanese[0].reading
     else:
         other_forms = []
         for j in data.japanese:
-            if j == word:
+            if j.word == word:
                 reading = j.reading
             else:
                 other_forms.append(j.word)
 
+    try:
+        print(word,reading)
+    except:
+        print(word)
+        print(other_forms)
+        sys.exit(0)
 
     if len(data.senses) == 1:
         english_def = data.senses[0].english_definitions
         pos = data.senses[0].parts_of_speech
     else:
-        ## TODO
-        pass
+        for i in range(len(data.senses)//2):
+            english_def = data.senses[i].english_definitions
+            pos = data.senses[i].parts_of_speech
+            res.append([word,reading,english_def,pos])
+        
 
-    return [word, reading, english_def, pos]
+    res.append([word, reading, english_def, pos])
+    return res
 
 def process_word_list(df):
     
@@ -36,9 +49,10 @@ def process_word_list(df):
 
 if __name__ == '__main__':
 
-    print("Processing word list")
+    print("Processing word list...")
 
     df = open_file('./data/words.csv')
     word_lookups = process_word_list(df)
-    lookup_df = pd.DataFrame(word_lookups, columns=['word', 'reading', 'english_def', 'pos'])
-    save_df(lookup_df, './data/queried_words.csv')
+    print(word_lookups)
+    #lookup_df = pd.DataFrame(word_lookups, columns=['word', 'reading', 'english_def', 'pos'])
+    #save_df(lookup_df, './data/queried_words.csv')
